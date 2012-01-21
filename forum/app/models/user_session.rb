45b83b3ef864ed0220039b78fed41cfaf2980614
Jiwon Kim <kittyvision@gmail.com>
2012-01-21 15:39:18 -0800

    added login and logout

--- forum/app/models/user_session.rb
+++ forum/app/models/user_session.rb
@@ -1,4 +1,9 @@
 class UserSession < Authlogic::Session::Base
-  # specify configuration here:
-  logout_on_timeout true
+  def to_key
+    new_record? ? nil : [ self.send(self.class.primary_key) ]
+  end
+
+  def persisted?
+    false
+  end
 end
